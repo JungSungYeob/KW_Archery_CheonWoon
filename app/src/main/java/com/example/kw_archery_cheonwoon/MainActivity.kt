e9f85da83ffa6708b7316528588ffb40149c1ad4
정성엽 <sungyeob@jeongseong-yeob-ui-MacBookPro.local>
2022-01-25 22:47:04 +0900

    Initial commit

---
+++ app/src/main/java/com/example/kw_archery_cheonwoon/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.kw_archery_cheonwoon
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
